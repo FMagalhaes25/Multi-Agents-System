439457d529b60616169e2579a68d068a97500d24
FMagalhaes25 <felipemostaco05@gmail.com>
2026-08-24 16:18:28 -0300

    update comms specialist

--- comms_specialist.ts
+++ comms_specialist.ts
@@ -4,7 +4,6 @@ import { State } from "./state.js"
 export const commsSpecialist = (state: typeof State.State) => {
     console.log("Comms Specialist chamado")
     return {
-        executedNodes: 1,
-        output: [new AIMessage("olá da IA")]
+        messages: [new AIMessage("olá da IA")]
     }
 }
