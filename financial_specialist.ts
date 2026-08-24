c823ee2697ae8130138778b27cf9778e0d67813d
FMagalhaes25 <felipemostaco05@gmail.com>
2026-08-24 15:47:14 -0300

    feat: financial specialist

---
+++ financial_specialist.ts
@@ -0,0 +1,10 @@
+import { AIMessage } from "@langchain/core/messages"
+import { State } from "./state.js"
+
+export const financialSpecialist = (state: typeof State.State) => {
+    console.log("Financial Specialist chamado")
+    return {
+        executedNodes: 1,
+        output: [new AIMessage("olá da IA")]
+    }
+}
