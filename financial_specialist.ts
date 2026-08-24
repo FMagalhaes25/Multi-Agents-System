a97c957f75161fc9bd4d67db91d17b2456ebcc82
FMagalhaes25 <felipemostaco05@gmail.com>
2026-08-24 16:18:38 -0300

    update financial specialist

--- financial_specialist.ts
+++ financial_specialist.ts
@@ -4,7 +4,6 @@ import { State } from "./state.js"
 export const financialSpecialist = (state: typeof State.State) => {
     console.log("Financial Specialist chamado")
     return {
-        executedNodes: 1,
-        output: [new AIMessage("olá da IA")]
+        messages: [new AIMessage("Aqui está sua conta: 300 reais")]
     }
 }
