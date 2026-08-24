3c887047db86b54072e19d09705b4919245d191b
FMagalhaes25 <felipemostaco05@gmail.com>
2026-08-24 16:19:36 -0300

    update scheduling specialist

--- scheduling_specialist.ts
+++ scheduling_specialist.ts
@@ -4,7 +4,6 @@ import { State } from "./state.js"
 export const schedulingSpecialist = (state: typeof State.State) => {
     console.log("Scheduling Specialist chamado")
     return {
-        executedNodes: 1,
-        output: [new AIMessage("olá da IA")]
+        messages: [new AIMessage("olá da IA")]
     }
 }
