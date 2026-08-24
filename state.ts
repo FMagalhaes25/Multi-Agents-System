1c995b2a58676ba6574b63d3f257f1d58018ee25
FMagalhaes25 <felipemostaco05@gmail.com>
2026-08-24 16:19:56 -0300

    feat: update states

--- state.ts
+++ state.ts
@@ -2,13 +2,8 @@ import { Annotation } from "@langchain/langgraph";
 import { BaseMessage,HumanMessage } from "@langchain/core/messages";
 
 export const State = Annotation.Root({
-    input: Annotation<HumanMessage>,
-    executedNodes: Annotation<number>({
-        reducer: (currExecuted, newOutput) => currExecuted + 1,
-        default: () => 0
-    }),
     nextNode: Annotation<string>, 
-    output: Annotation<BaseMessage[]>({
+    messages: Annotation<BaseMessage[]>({
         reducer: (currOutput, newOutput) => currOutput.concat(newOutput),
         default: () => []
     }),
